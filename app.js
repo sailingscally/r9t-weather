1716c94fb73407ee2681bd972aa6b02687a7188d
Luis Martins <luis.martins@gmail.com>
2022-11-05 03:23:30 +0000

    Refactoring 'app.js'.

--- app.js
+++ app.js
@@ -22,62 +22,58 @@
 const bme280 = require('bme280');
 const mqtt = require('mqtt');
 
-const run = async () => {
-  console.log('Starting R nineT weather station...')
+console.log('Starting R nineT weather station...')
 
-  const client = mqtt.connect('mqtt://localhost:1883', {
-    clientId: 'r9t-weather'
-  });
+const client = mqtt.connect('mqtt://localhost:1883', {
+  clientId: 'r9t-weather'
+});
 
-  let connected = false;
+let connected = false;
 
-  client.on('connect', () => {
-    console.log('Connected to MQTT broker.');
+client.on('connect', () => {
+  console.log('Connected to MQTT broker.');
 
-    client.options.reconnectPeriod = 1000;
-    connected = true;
-  });
+  client.options.reconnectPeriod = 1000;
+  connected = true;
+});
 
-  client.on('close', () => {
-    if(connected) {
-      console.log('Connection to MQTT broker lost.');
-      connected = false;
-    }
-  });
+client.on('close', () => {
+  if(connected) {
+    console.log('Connection to MQTT broker lost.');
+    connected = false;
+  }
+});
 
-  client.on('error', (error) => {
-    switch(error.code) {
-      case 'ECONNREFUSED':
-        console.log(`Unable to connect to MQTT broker on ${error.address}:${error.port}.`);
-        break;
+client.on('error', (error) => {
+  switch(error.code) {
+    case 'ECONNREFUSED':
+      console.log(`Unable to connect to MQTT broker on ${error.address}:${error.port}.`);
+      break;
 
-      default:
-        console.log(error);
-        break;
-    }
-  });
+    default:
+      console.log(error);
+      break;
+  }
+});
 
-  /*
-   * Increase the time between connection attempts
-   */
-  client.on('reconnect', () => {
-    client.options.reconnectPeriod *= 2;
-  });
+/*
+ * Increase the time between connection attempts
+ */
+client.on('reconnect', () => {
+  client.options.reconnectPeriod *= 2;
+});
 
-  const job = setInterval(async () => {
-    const sensor = await bme280.open();
-    const data = await sensor.read();
-    await sensor.close();
+setInterval(async () => {
+  const sensor = await bme280.open();
+  const data = await sensor.read();
+  await sensor.close();
 
-    console.log('Weather [t/p/h]: ' + data.temperature.toFixed(1) + 'º / ' +
-      data.pressure.toFixed(0) + 'hPa / ' + data.humidity.toFixed(0) + '%');
+  console.log('Weather [t/p/h]: ' + data.temperature.toFixed(1) + 'º / ' +
+    data.pressure.toFixed(0) + 'hPa / ' + data.humidity.toFixed(0) + '%');
 
-    if(connected) {
-      client.publish('weather/temperature', data.temperature.toString());
-      client.publish('weather/pressure', data.pressure.toString());
-      client.publish('weather/humidity', data.humidity.toString());
-    }
-  }, 10000);
-}
-
-run();
+  if(connected) {
+    client.publish('weather/temperature', data.temperature.toString());
+    client.publish('weather/pressure', data.pressure.toString());
+    client.publish('weather/humidity', data.humidity.toString());
+  }
+}, 10000);
